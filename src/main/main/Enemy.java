51331e92889041bd4cad7743f63f5132f2a1b7b3
namPh06 <phanhoainam19062005@gmail.com>
2025-11-02 15:15:48 +0700

    Update hp & size boss

--- src/main/main/Enemy.java
+++ src/main/main/Enemy.java
@@ -96,12 +96,12 @@ public class Enemy {
                 break;
             case BOSS:
                 this.speed = baseSpeed * 0.4;
-                this.maxHP = baseHP * 15 + (level * 8);
+                this.maxHP = baseHP * 5 + (level * 5);
                 this.damage = baseDamage * 4 + (level / 2);
                 this.scoreValue = 200 * level;
                 // ★★★ BOSS TO HƠN ★★★
-                this.width = 120;  // Tăng từ 90 lên 120
-                this.height = 120; // Tăng từ 90 lên 120
+                this.width = 150;  // Tăng từ 90 lên 120
+                this.height = 150; // Tăng từ 90 lên 120
                 break;
         }
         
